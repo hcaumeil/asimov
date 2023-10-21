ad712ccc75a82418e09e0d901f69e62895a38649
Xess1407 <evanclausse@yahoo.fr>
2023-10-21 17:04:48 +0200

    login page

---
+++ src/main/kotlin/com/sud/asimov/pages/Login.kt
@@ -0,0 +1,33 @@
+package com.sud.asimov.pages
+
+import com.sud.asimov.User
+import com.sud.asimov.pages.dto.LoginDTO
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+
+import org.springframework.web.bind.annotation.PostMapping
+
+@Controller
+class Login {
+    @GetMapping("/login")
+    fun categories(model : Model) : String {
+        model.addAttribute("loginDTO", LoginDTO())
+        return "login"
+    }
+
+    @PostMapping("/login")
+    fun greetingSubmit(@ModelAttribute loginData : LoginDTO, model: Model): String {
+        model.addAttribute("loginDTO", loginData) // debug
+
+        // verifie email / password
+        if (loginData.email == "a") // debug
+        // Return error
+        return "login"
+
+        // Return index
+        return "index"
+    }
+}
---
+++ src/main/kotlin/com/sud/asimov/pages/dto/LoginDTO.kt
@@ -0,0 +1,7 @@
+package com.sud.asimov.pages.dto
+
+class LoginDTO (
+    val email: String = "null",
+    val password : String = "null"
+) {
+}
