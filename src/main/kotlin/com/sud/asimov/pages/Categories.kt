301cefeceaddcdacc50b6b417723ee7ed911d2cf
Xess1407 <evanclausse@yahoo.fr>
2023-10-21 15:57:06 +0200

    list categories name

--- src/main/kotlin/com/sud/asimov/pages/Categories.kt
+++ src/main/kotlin/com/sud/asimov/pages/Categories.kt
@@ -1,11 +1,24 @@
 package com.sud.asimov.pages
 
+import com.sud.asimov.Category
+import com.sud.asimov.CategoryController
+import com.sud.asimov.CategoryRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+
+
+
 
 @Controller
 class Categories {
+    @Autowired
+    lateinit var repository : CategoryRepository;
     @GetMapping("/categories")
-    fun categories(model : Model) : String = "categories"
+    fun categories(model : Model) : String {
+        model.addAttribute("categories", repository.findAll())
+        return "categories"
+    }
 }
