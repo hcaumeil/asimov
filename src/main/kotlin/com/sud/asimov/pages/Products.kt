1ca4075e04087f246bed0f858ff926e8b5714edd
Xess1407 <evanclausse@yahoo.fr>
2023-11-03 12:59:32 +0100

    save cart products db

--- src/main/kotlin/com/sud/asimov/pages/Products.kt
+++ src/main/kotlin/com/sud/asimov/pages/Products.kt
@@ -3,6 +3,9 @@ package com.sud.asimov.pages
 import com.sud.asimov.Category
 import com.sud.asimov.Product
 import com.sud.asimov.ProductRepository
+import com.sud.asimov.UserRepository
+import com.sud.asimov.api.cartproduct.CartProductRepository
+import com.sud.asimov.api.entities.CartProduct
 import com.sud.asimov.pages.dto.ProductCommandDTO
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,16 +14,22 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import kotlin.jvm.optionals.getOrNull
 
 @Controller
 class Products {
     @Autowired
-    lateinit var repository : ProductRepository
+    lateinit var userRepository : UserRepository;
+
+    @Autowired
+    lateinit var productsRepository : ProductRepository;
+    @Autowired
+    lateinit var cartProductRepository : CartProductRepository
     @GetMapping("/products")
     @Transactional
     fun products(@RequestParam categoryName : String, model : Model): String {
         // Get all products from category
-        val products : List<Product> = repository.findByCategory(Category(categoryName))
+        val products : List<Product> = productsRepository.findByCategory(Category(categoryName))
         // Set the model with data
         model.addAttribute("category", categoryName)
         model.addAttribute("products", products)
@@ -30,9 +39,13 @@ class Products {
     @PostMapping("/products/add-cart")
     @Transactional
     fun productCommandSubmit(@RequestBody productCommandData : ProductCommandDTO, model: Model): ResponseEntity<String> {
-        // Traitement
-        // Erreur inattendu ?
-        //model.addAttribute("message", "Erreur : Un problème serveur est survenu")
+        val u = userRepository.findById(productCommandData.userId)
+        if (u.isEmpty) return ResponseEntity.badRequest().body("User not found")
+
+        val p = productsRepository.findById(productCommandData.productId)
+        if (p.isEmpty) return ResponseEntity.badRequest().body("Product not found")
+
+        cartProductRepository.save(CartProduct(null, u.get(), p.get(), productCommandData.quantity))
 
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("Commande pris en compte")
     }
