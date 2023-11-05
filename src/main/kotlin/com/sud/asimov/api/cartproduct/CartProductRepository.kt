0108213114618edc3c26afde5426d6022cc941e7
Xess1407 <evanclausse@yahoo.fr>
2023-11-05 09:52:14 +0100

    cart delete product

--- src/main/kotlin/com/sud/asimov/api/cartproduct/CartProductRepository.kt
+++ src/main/kotlin/com/sud/asimov/api/cartproduct/CartProductRepository.kt
@@ -3,7 +3,7 @@ package com.sud.asimov.api.cartproduct
 import com.sud.asimov.api.entities.CartProduct
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CartProductRepository : JpaRepository<CartProduct, String> {
+interface CartProductRepository : JpaRepository<CartProduct, Long> {
     fun findByUser_Id(user: Long): CartProduct?
     fun findAllByUserId(userId: Long) : List<CartProduct?>
     fun deleteCartProductByProductId(productId: Long)
--- src/main/kotlin/com/sud/asimov/pages/Cart.kt
+++ src/main/kotlin/com/sud/asimov/pages/Cart.kt
@@ -5,6 +5,8 @@ import com.sud.asimov.pages.dto.LoginDTO
 import com.sud.asimov.pages.dto.ProductAddDTO
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -23,4 +25,16 @@ class Cart {
         return "cart"
     }
 
+    @PostMapping("/cart/delete")
+    @Transactional
+    fun productDeleteSubmit(@RequestBody id : Long, model: Model): ResponseEntity<String> {
+        repository.deleteById(id)
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("Produit supprimé")
+    }
+
+    @GetMapping("/cart/fragment/product/{id}")
+    fun showProductPart(@PathVariable("id") id: Long,model : Model) : String {
+        model.addAttribute("items", repository.findAllByUserId(id))
+        return "cart :: #cart-product-table"
+    }
 }
