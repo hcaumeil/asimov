6e45df4f9cda5cf768dc26665696021e20503aee
Xess1407 <evanclausse@yahoo.fr>
2023-11-05 19:24:08 +0100

    error page

--- src/main/kotlin/com/sud/asimov/pages/Errors.kt
+++ src/main/kotlin/com/sud/asimov/pages/Errors.kt
@@ -1,6 +1,5 @@
 package com.sud.asimov.pages
 
-import com.sud.asimov.pages.dto.ContactDTO
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping
 class Errors {
     @GetMapping("/404")
     fun error404(model : Model) : String {
-        return "404"
+        return "error"
     }
     @GetMapping("/403")
     fun error403(model : Model) : String {
